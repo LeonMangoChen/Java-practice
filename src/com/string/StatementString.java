56c73a17fd052e312499f2f2962d14b900f48418
LeonMangoChen <leonmangochen@163.com>
2018-05-01 15:54:26 +0800

    解决中文乱码问题

--- src/com/string/StatementString.java
+++ src/com/string/StatementString.java
@@ -1,10 +1,10 @@
-package com.string;
+ï»¿package com.string;
 
 public class StatementString {
 
 	public static void main(String[] args) {
-		// ¶¨Òå×Ö·û´®
-		String hobby = "ÍøÖ·";
+		// å®šä¹‰å­—ç¬¦ä¸²
+		String hobby = "ç½‘å€";
 		String url = "www.example.com";
 		System.out.println("hobby:" + hobby);
 		System.out.println("url:" + url);
